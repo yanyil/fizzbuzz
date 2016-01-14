7d6ea77ca2ad870d96e0a697665bc091a6ec97b5
Yan-Yi Li <liyanyi@gmail.com>
2016-01-14 14:04:29 +0000

    1..20 tests pass

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz number
-  return "fizzbuzz" if number == 15
-  return "buzz" if number == 5
-  return "fizz" if number == 3
+  return "fizzbuzz" if number %  15 == 0
+  return "buzz" if number % 5 == 0
+  return "fizz" if number % 3 == 0
   number
 
 end
